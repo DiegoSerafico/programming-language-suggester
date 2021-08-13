5a9f114314d321a62ec8909a7df20822b26a5878
DiegoSerafico <dseraficohernandez@gmail.com>
2021-08-13 13:57:34 -0700

    add descriptions for results

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,37 @@
+const javascriptDescription = "Since you said you are interested in function, I recommend you try Javascript! Javascript is used to give websites functionality.";
+const cssDescription = "Since you are interested in design you should learn css! Css is used to add styling to a webpage. You can use it to update colors, fonts, spacing, and more. While you're at it, check out HTML for creating the elements on the page!";
+const scratchDescription = "You said you are younger then 12. Wow, you're a rockstar! I recommend you start with Scratch. Its a fun way to learn coding and build games!";
+const swiftDescription = "You said you're an Apple person. I recommend you use swift. Its the language used to build iOS apps!";
+const pythonDescription = "Ah, I see you seek the holy grail! Python will help you in your quest!";
+
 function outputPython(name) {
   $("#nameOutput").text(name);
   $("#firstLanguage").text("Python!");
+  $("#languageDescription").text(pythonDescription);
 }
 
 function outputApple(name) {
   $("#nameOutput").text(name);
   $("#firstLanguage").text("Swift!");
+  $("#languageDescription").text(swiftDescription);
 }
 
 function outputJavaScript(name) {
   $("#nameOutput").text(name);
   $("#firstLanguage").text("JavaScript!");
+  $("#languageDescription").text(javascriptDescription);
 }
 
 function outputCss(name) {
   $("#nameOutput").text(name);
   $("#firstLanguage").text("CSS!");
+  $("#languageDescription").text(cssDescription);
 }
 
 function outputScratch(name) {
   $("#nameOutput").text(name);
   $("#firstLanguage").text("Scratch!");
+  $("#languageDescription").text(scratchDescription);
 }
 
 $(document).ready(function() {
@@ -35,7 +46,7 @@ $(document).ready(function() {
     const platformChoice = $("#platformChoice").val();
     const purposeChoice = $("#purposeChoice").val();
 
-    if (nameInput === "" || isNaN(ageInput) || isNaN(swallowSpeed) || ageInput < 1 || swallowSpeed < 1) {
+    if (nameInput === "" || isNaN(ageInput) || isNaN(swallowSpeed) || ageInput < 0 || swallowSpeed < 0) {
       alert("One or more of your answers may not be valid. Please check your answers and try again");
     }
 
