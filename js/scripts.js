292bce0f6688f93a06e0066d5852af4b327550c3
DiegoSerafico <dseraficohernandez@gmail.com>
2021-08-13 10:19:31 -0700

    add validation for user inputs on form

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,50 @@ function outputPython(name) {
 
 function outputApple(name) {
   $("#nameOutput").text(name);
-  $("#fistLanguage").text("Apple!");
+  $("#firstLanguage").text("Swift!");
 }
 
 function outputJavaScript(name) {
   $("#nameOutput").text(name);
-  $("#fistLanguage").text("JavaScript!");
+  $("#firstLanguage").text("JavaScript!");
 }
 
 function outputCss(name) {
   $("#nameOutput").text(name);
-  $("#fistLanguage").text("CSS!");
+  $("#firstLanguage").text("CSS!");
+}
+
+function outputScratch(name) {
+  $("#nameOutput").text(name);
+  $("#firstLanguage").text("Scratch!");
 }
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     $("#output").hide();
+    $("#nameOutput").empty();
+    $("#firstLanguage").empty();
     const nameInput = $("#nameInput").val();
-    const ageInput = $("#ageInput").val();
+    const ageInput = parseInt($("#ageInput").val());
     const swallowSpeed = parseInt($("#swallowSpeed").val());
     const platformChoice = $("#platformChoice").val();
     const purposeChoice = $("#purposeChoice").val();
 
+    if (nameInput === "" || isNaN(ageInput) || isNaN(swallowSpeed) || ageInput < 1 || swallowSpeed < 1) {
+      alert("One or more of your answers may not be valid. Please check your answers and try again");
+    }
+
     if (swallowSpeed === 24) {
       outputPython(nameInput);
-    } else if (platformChoice === "Apple") {
-      outputApple(nameInput);
+    } else if (ageInput < 12) {
+      outputScratch(nameInput);
     } else if (platformChoice === "Windows" && purposeChoice === "Function") {
       outputJavaScript(nameInput);
     } else if (platformChoice === "Windows" && purposeChoice === "Design") {
       outputCss(nameInput);
+    } else {
+      outputApple(nameInput);
     }
 
     $("#output").show();
